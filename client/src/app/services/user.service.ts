8642227dd2cfa531abdc27b0ac963bfaad5adedb
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-13 12:22:29 +0330

    complete user service

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoggedIn } from '../models/logged-in.model';
+import { AppUser } from '../models/app-user.model';
+import { login } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  register(user: AppUser): Observable<LoggedIn> {
+    return this.http.post<LoggedIn>('http://localhost:5000/api/user/create', user);
+  }
+
+  login(userInput: login): Observable<LoggedIn> {
+    return this.http.post<LoggedIn>('http://localhost:5000/api/user/login', userInput);
+  }
 }
