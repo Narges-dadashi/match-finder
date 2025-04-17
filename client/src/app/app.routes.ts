6fec3a687492184feb398c51ab26656aff12e936
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-17 21:29:08 +0330

    Create Components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'account/register', component: RegisterComponent },
+    { path: 'account/login', component: LoginComponent },
+    { path: 'footer', component: FooterComponent },
+    { path: 'navbar', component: NavbarComponent },
+    { path: '**', component: NotFoundComponent }
+];
