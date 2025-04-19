0f18e4c80b3e453cb47013d12ad84d1f5aa9d182
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-19 10:49:51 +0330

    Register Component

--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AccountService } from '../../../services/account.service';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppUser } from '../../../models/app-user.model';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule, ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  accountService = inject(AccountService);
+  fB = inject(FormBuilder);
 
+  registerFg = this.fB.group({
+    emailCtrl: ['', [Validators.required, Validators.email]],
+    userNameCtrl: '',
+    passwordCtrl: '',
+    confirmPasswordCtrl: '',
+    ageCtrl: 0,
+    cityCtrl: '',
+    countryCtrl: ''
+  })
+
+  get EmailCtrl(): FormControl {
+    return this.registerFg.get('emailCtrl') as FormControl;
+  }
+
+  get UserNameCtrl(): FormControl {
+    return this.registerFg.get('userNameCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.registerFg.get('passwordCtrl') as FormControl;
+  }
+
+  get ConfirmPasswordCtrl(): FormControl {
+    return this.registerFg.get('confirmPasswordCtrl') as FormControl;
+  }
+
+  get AgeCtrl(): FormControl {
+    return this.registerFg.get('ageCtrl') as FormControl;
+  }
+
+  get CityCtrl(): FormControl {
+    return this.registerFg.get('cityCtrl') as FormControl;
+  }
+
+  get CountryCtrl(): FormControl {
+    return this.registerFg.get('countryCtrl') as FormControl;
+  }
+
+  register(): void {
+    let user: AppUser = {
+      email: this.EmailCtrl.value,
+      userName: this.UserNameCtrl.value,
+      password: this.PasswordCtrl.value,
+      confirmPassword: this.ConfirmPasswordCtrl.value,
+      age: this.AgeCtrl.value,
+      city: this.CityCtrl.value,
+      country: this.CountryCtrl.value
+    }
+
+    this.accountService.register(user).subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err.error)
+    });
+  }
 }
