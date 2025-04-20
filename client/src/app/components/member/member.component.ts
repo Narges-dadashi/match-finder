661918260a4bc7ff35993d439949df0ec83baf99
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-20 10:37:50 +0330

    ngOnInit for getAllMembers

--- client/src/app/components/member/member.component.ts
+++ client/src/app/components/member/member.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AccountService } from '../../services/account.service';
+import { Member } from '../../models/member.model';
 
 @Component({
   selector: 'app-member',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss'
 })
 export class MemberComponent {
+  accountService = inject(AccountService);
+  members: Member[] | undefined;
 
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(): void {
+    this.accountService.getAllMember().subscribe({
+      next: (res) => {
+        this.members = res;
+      }
+    });
+  }
 }
