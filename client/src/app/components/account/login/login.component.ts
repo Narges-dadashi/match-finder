531f0369a67afbfa922a7bfd1698aeae20dfd196
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-24 10:09:05 +0330

    login.component

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -6,12 +6,14 @@ import { RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   imports: [
     RouterLink,
     FormsModule, ReactiveFormsModule,
+    MatCardModule,
     MatFormFieldModule, MatInputModule, MatButtonModule
   ],
   templateUrl: './login.component.html',
