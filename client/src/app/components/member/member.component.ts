cee6244bff8061e82611039a8b2f9705c4917e47
Narges-dadashi <nargesdadashi7@gmail.com>
2025-04-26 10:46:37 +0330

    edit member ts

--- client/src/app/components/member/member.component.ts
+++ client/src/app/components/member/member.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AccountService } from '../../services/account.service';
 import { Member } from '../../models/member.model';
@@ -12,7 +12,7 @@ import { Member } from '../../models/member.model';
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss'
 })
-export class MemberComponent {
+export class MemberComponent implements OnInit {
   accountService = inject(AccountService);
   members: Member[] | undefined;
 
