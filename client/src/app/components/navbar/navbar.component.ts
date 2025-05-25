a9e6dd2e4a5987efb3bee4edbcd2bdd203ef8fea
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-25 12:19:49 +0330

    logout in Navbar.ts

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { AccountService } from '../../services/account.service';
 
 @Component({
     selector: 'app-navbar',
@@ -15,5 +16,9 @@ import { RouterLink } from '@angular/router';
     styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    accountService = inject(AccountService);
 
+    logout(): void {
+        this.accountService.logout();
+    }
 }
