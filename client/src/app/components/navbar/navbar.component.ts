57c6678bdf8c2cf88f217166de47249eb50ca4ea
Narges-dadashi <nargesdadashi7@gmail.com>
2025-06-05 16:33:51 +0330

    Add Menu in Navbar

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterLink } from '@angular/router';
 import { AccountService } from '../../services/account.service';
 import { ExampleService } from '../../services/example.service';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     imports: [
-        RouterLink,
-        MatToolbarModule, MatIconModule, MatButtonModule
+        RouterLink, CommonModule,
+        MatToolbarModule, MatIconModule,
+        MatButtonModule, MatMenuModule,
+        MatDividerModule, MatListModule
     ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss'
