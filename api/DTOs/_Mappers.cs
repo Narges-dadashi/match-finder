f0930ce931d744c4df887c781126325c216095a1
Narges-dadashi <nargesdadashi7@gmail.com>
2025-07-01 12:37:12 +0330

    ConvertAppUserToMemberDto In Mappers

--- api/DTOs/_Mappers.cs
+++ api/DTOs/_Mappers.cs
@@ -1,12 +1,23 @@
 namespace api.DTOs;
 
-public static class _Mappers
+public static class Mappers
 {
     public static LoggedInDto ConvertAppUserToLoggedInDto(AppUser appUser)
     {
         return new(
             Email: appUser.Email,
             UserName: appUser.UserName
+        );
+    }
+
+    public static MemberDto ConvertAppUserToMemberDto(AppUser appUser)
+    {
+        return new(
+            Email: appUser.Email,
+            UserName: appUser.UserName,
+            Age: appUser.Age,
+            City: appUser.City,
+            Country: appUser.Country
         );
     }
 }
