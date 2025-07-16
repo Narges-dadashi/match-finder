d5720f5c2f1d3d5c6b72f6907f34bfba7c3706ec
Narges-dadashi <nargesdadashi7@gmail.com>
2025-07-16 12:00:44 +0330

    Token in Mapper

--- api/DTOs/_Mappers.cs
+++ api/DTOs/_Mappers.cs
@@ -2,11 +2,12 @@ namespace api.DTOs;
 
 public static class Mappers
 {
-    public static LoggedInDto ConvertAppUserToLoggedInDto(AppUser appUser)
+    public static LoggedInDto ConvertAppUserToLoggedInDto(AppUser appUser, string tokenValue)
     {
         return new(
             Email: appUser.Email,
-            UserName: appUser.UserName
+            UserName: appUser.UserName,
+            Token: tokenValue
         );
     }
 
