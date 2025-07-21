37ba1db621756d20afb7c9b8874a16bb80a0e045
Narges-dadashi <nargesdadashi7@gmail.com>
2025-07-21 12:06:26 +0330

    Add JWT Interceptors to app.config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,13 +5,13 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
-// import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { jwtInterceptor } from './interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    // provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
