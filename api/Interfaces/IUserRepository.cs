a56098f945a982318f7721d96293e515dea5f9c9
Narges-dadashi <nargesdadashi7@gmail.com>
2025-09-01 19:38:46 +0330

    Add SetMainPhoto & DaletePhoto in IUserRepository

--- api/Interfaces/IUserRepository.cs
+++ api/Interfaces/IUserRepository.cs
@@ -4,5 +4,7 @@ public interface IUserRepository
 {
     public Task<AppUser?> GetByIdAsync(string userId, CancellationToken cancellationToken);
     public Task<MemberDto?> UpdateByIdAsync(string userId, AppUser userInput, CancellationToken cancellationToken);
-    public Task<Photo?> UploadPhotoAsync(IFormFile formFile, string userId, CancellationToken cancellationToken);
+    public Task<Photo?> UploadPhotoAsync(IFormFile file, string userId, CancellationToken cancellationToken);
+    public Task<UpdateResult?> SetMainPhotoAsync(string hashedUserId, string photoUrlIn, CancellationToken cancellationToken);
+    public Task<UpdateResult?> DeletePhotoAsync(string hashedUserId, string? urlIn, CancellationToken cancellationToken);
 }
