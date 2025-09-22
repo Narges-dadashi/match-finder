27b2122e0e01be942c3f0d50786e5f6a5005c632
Narges-dadashi <nargesdadashi7@gmail.com>
2025-09-22 11:23:43 +0330

    Create Member-list

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Member } from '../../../models/member.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MemberService } from '../../../services/member.service';
+import { Observable } from 'rxjs';
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatCardModule, MatIconModule, MemberCardComponent
+  ],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
 export class MemberListComponent {
+  memberService = inject(MemberService);
+  members: Member[] | undefined;
 
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(): void {
+    let allMember$: Observable<Member[]> = this.memberService.getAllMembers();
+
+    allMember$.subscribe({
+      next: (res) => {
+        this.members = res;
+        console.log(res);
+      }
+    });
+  }
 }
