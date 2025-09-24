5837969a48cc7bb54f1e36abceac31c2765b1fd6
Narges-dadashi <nargesdadashi7@gmail.com>
2025-09-24 17:07:43 +0330

    Add user-card in user-edit

--- client/src/app/components/user/user-edit/user-edit.component.ts
+++ client/src/app/components/user/user-edit/user-edit.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { MemberService } from '../../../services/member.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+// import { PhotoEditorComponent } from '../photo-editor/photo-editor.component';
 import { environment } from '../../../../environments/environment.development';
 import { Member } from '../../../models/member.model';
-import { isPlatformBrowser } from '@angular/common';
 import { LoggedIn } from '../../../models/logged-in.model';
+import { MemberService } from '../../../services/member.service';
 import { take } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-edit',
-  imports: [],
+  imports: [
+    MatCardModule, MatTabsModule, CommonModule, MatButtonModule
+  ],
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.scss'
 })
