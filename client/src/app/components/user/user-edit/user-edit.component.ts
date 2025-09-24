358bcd1b139abac132c3e0baa7cf2af6348f7537
Narges-dadashi <nargesdadashi7@gmail.com>
2025-09-24 16:59:01 +0330

    getMember in user-edit.ts

---
+++ client/src/app/components/user/user-edit/user-edit.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { MemberService } from '../../../services/member.service';
+import { environment } from '../../../../environments/environment.development';
+import { Member } from '../../../models/member.model';
+import { isPlatformBrowser } from '@angular/common';
+import { LoggedIn } from '../../../models/logged-in.model';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'app-user-edit',
+  imports: [],
+  templateUrl: './user-edit.component.html',
+  styleUrl: './user-edit.component.scss'
+})
+export class UserEditComponent implements OnInit {
+  private _platformId = inject(PLATFORM_ID);
+  private _memberService = inject(MemberService);
+
+  apiUrl = environment.apiUrl;
+  member: Member | undefined;
+
+  ngOnInit(): void {
+    this.getMember();
+  }
+
+  getMember(): void {
+    if (isPlatformBrowser(this._platformId)) {
+      const loggedInUserStr: string | null = localStorage.getItem('loggedInUser');
+
+      if (loggedInUserStr) {
+        const loggedInUser: LoggedIn = JSON.parse(loggedInUserStr);
+
+        this._memberService.getByUserName(loggedInUser.userName).pipe(take(1)).subscribe({
+          next: (res) => {
+            this.member = res;
+          }
+        })
+      }
+    }
+  }
+}
