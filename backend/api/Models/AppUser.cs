24c9c16400dba89c74b610af7d1b9f2a760d615f
Narges-dadashi <nargesdadashi7@gmail.com>
2025-10-29 22:24:33 +0330

    Edit image-proccessing

--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -23,28 +23,3 @@ public class AppUser
     public string Country { get; init; } = string.Empty;
     public List<Photo> Photos { get; init; } = [];
 }
-
-
-
-
-
-// namespace api.Models;
-
-// public record AppUser(
-//     [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
-//     string? Id,
-//     [EmailAddress] string Email,
-//     string UserName,
-//     string Password,
-//     string ConfirmPassword,
-//     [Range(typeof(DateOnly), "1900-01-01", "2050-01-01", ErrorMessage = "Date of birth must be between 1900 and 2050.")]
-//     DateOnly DateOfBirth,
-//     DateTime LastActive,
-//     string Gender,
-//     string Introduction,
-//     string LookingFor,
-//     string Interests,
-//     string City,
-//     string Country,
-//     List<Photo> Photos
-// );
