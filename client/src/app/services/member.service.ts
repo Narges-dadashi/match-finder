f56ea89a80ba510731bf660e22d059943fdd85d8
Narges-dadashi <nargesdadashi7@gmail.com>
2025-11-15 22:25:42 +0330

    Add Pagination To memberService

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Member } from '../models/member.model';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
+import { PaginationParams } from '../models/helpers/paginationParams.model';
+import { PaginatedResult } from '../models/helpers/paginatedResult';
+import { PaginationHandler } from '../extensions/paginationHandler';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +14,18 @@ export class MemberService {
   http = inject(HttpClient);
 
   private readonly _baseApiUrl: string = environment.apiUrl + 'api/member/';
+  private _paginationHandler = new PaginationHandler();
 
-  getAllMembers(): Observable<Member[]> {
+  getAllMembers(paginationParams: PaginationParams): Observable<PaginatedResult<Member[]>> {
+    let params = new HttpParams();
 
-    let members$: Observable<Member[]>
-      = this.http.get<Member[]>(this._baseApiUrl + 'get-all');
+    params = params.append('pageSize', paginationParams.pageSize);
+    params = params.append('pageNumber', paginationParams.pageNumber);
 
-    return members$;
+    return this._paginationHandler.getPaginatedResult<Member[]>(this._baseApiUrl + 'get-all', params);
   }
 
   getByUserName(userNameInput: string): Observable<Member | undefined> {
     return this.http.get<Member>(this._baseApiUrl + 'get-by-username/' + userNameInput);
   }
-}
+} 
