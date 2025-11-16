92197d39979620cedc5e0ed90b0d2fb4ef0385f0
Narges-dadashi <nargesdadashi7@gmail.com>
2025-11-16 23:39:13 +0330

    Use Pagination in member.component

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -4,34 +4,59 @@ import { Member } from '../../../models/member.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MemberService } from '../../../services/member.service';
-import { Observable } from 'rxjs';
 import { MemberCardComponent } from '../member-card/member-card.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PaginationParams } from '../../../models/helpers/paginationParams.model';
+import { PaginatedResult } from '../../../models/helpers/paginatedResult';
+import { Pagination } from '../../../models/helpers/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
   imports: [
-    MatCardModule, MatIconModule, MemberCardComponent
+    RouterLink,
+    MatCardModule, MatIconModule, MemberCardComponent,
+    MatPaginatorModule
   ],
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.scss']
+  styleUrl: './member-list.component.scss'
 })
 export class MemberListComponent {
   memberService = inject(MemberService);
   members: Member[] | undefined;
+  paginationParams: PaginationParams | undefined;
+  pageSizeOptions = [5, 10, 25];
+  pageEvent: PageEvent | undefined;
+  pagination: Pagination | undefined;
 
   ngOnInit(): void {
+    this.paginationParams = new PaginationParams();
+
     this.getAll();
   }
 
   getAll(): void {
-    let allMember$: Observable<Member[]> = this.memberService.getAllMembers();
+    if (this.paginationParams)
+      this.memberService.getAllMembers(this.paginationParams).subscribe({
+        next: (response: PaginatedResult<Member[]>) => {
+          if (response.body && response.pagination) {
+            this.members = response.body;
+            this.pagination = response.pagination;
+          }
+        }
+      })
+  }
 
-    allMember$.subscribe({
-      next: (res) => {
-        this.members = res;
-        console.log(res);
-      }
-    });
+  handlePageEvent(e: PageEvent): void {
+    if (this.paginationParams) {
+      if (e.pageSize !== this.paginationParams.pageSize)
+        e.pageIndex = 0;
+
+      this.pageEvent = e;
+      this.paginationParams.pageSize = e.pageSize;
+      this.paginationParams.pageNumber = e.pageIndex + 1;
+
+      this.getAll();
+    }
   }
 }
