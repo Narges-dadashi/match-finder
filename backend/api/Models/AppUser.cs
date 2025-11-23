8c13b75d1368044bcb04d92ec431a12ee8cf0092
Narges-dadashi <nargesdadashi7@gmail.com>
2025-11-23 20:41:21 +0330

    Edit AppUser

--- backend/api/Models/AppUser.cs
+++ backend/api/Models/AppUser.cs
@@ -1,16 +1,8 @@
 namespace api.Models;
 
-public class AppUser
+[CollectionName("users")]
+public class AppUser : MongoIdentityUser<ObjectId>
 {
-    [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; init; }
-    [EmailAddress]
-    public string Email { get; init; } = string.Empty;
-    [StringLength(30, MinimumLength = 3)]
-    public string UserName { get; init; } = string.Empty;
-    [StringLength(16, MinimumLength = 8)]
-    public string Password { get; init; } = string.Empty;
-    public string ConfirmPassword { get; init; } = string.Empty;
     [Range(typeof(DateOnly), "1900-01-01", "2050-01-01",
         ErrorMessage = "Date of birth must be between 1900 and 2050.")]
     public DateOnly DateOfBirth { get; init; }
